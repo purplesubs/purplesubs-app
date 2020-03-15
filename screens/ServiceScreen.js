cce7cf480a9f0e4530d6a6ab27b05ea977f8de53
Liván Frómeta <llfrometa@gmail.com>
2020-03-15 23:03:34 +0100

    Add Service List structure

--- screens/ServiceScreen.js
+++ screens/ServiceScreen.js
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {Image, Platform, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import SimpleTabBar from '../components/SimpleTabBar'
+import ServiceList from '../components/ServiceList'
 import {colors, display, textStyles} from '../constants/StyleSheet'
 
 
@@ -34,11 +35,39 @@ export default function ServiceScreen() {
         onPress: () => alert("alert All")
     }];
 
+    let services = [{
+        id: 'netflix',
+        name: 'Netflix',
+        image: require('../assets/images/services/netflix.png'),
+    },{
+        id: 'apple',
+        name: 'Apple',
+        image: require('../assets/images/services/apple.png'),
+    },{
+        id: 'spotify',
+        name: 'Spotify',
+        image: require('../assets/images/services/spotify.png'),
+    },{
+        id: 'trello',
+        name: 'Trello',
+        image: require('../assets/images/services/trello.png'),
+    },{
+        id: 'google',
+        name: 'Google',
+        image: require('../assets/images/services/google.png'),
+    }]
+
+    console.info("////////services>>>", services)
     return (
         <View style={styles.container}>
             <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
                 <Text style={textStyles.mainTitle}>Services</Text>
                 <SimpleTabBar tabs={planTypes}/>
+
+                <ServiceList>
+                    {services.map(item => <ServiceList.Item key={item.id} {...item}/>)}
+                </ServiceList>
+
             </ScrollView>
         </View>
     );
